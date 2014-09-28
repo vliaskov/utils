ec6e9afc583d01a0e4c76a86f5e26d9f18f38073
Vasilis Liaskovitis <vliaskov@gmail.com>
2014-09-28 15:18:24 +0200

    add rpi simple scripts, add gstreamer debugging option

---
+++ rpi/mount-sshfs.sh
@@ -0,0 +1,1 @@
+sudo sshfs pi@192.168.1.89:/ /opt/devel/rpi/home/ -o transform_symlinks -o allow_other
---
+++ rpi/prepare-chroot.sh
@@ -0,0 +1,4 @@
+sudo mount --rbind /dev /media/vliaskov/fc254b57-8fff-4f96-9609-ea202d871acf/dev/
+sudo mount --rbind /proc /media/vliaskov/fc254b57-8fff-4f96-9609-ea202d871acf/proc/
+sudo mount --rbind /sys /media/vliaskov/fc254b57-8fff-4f96-9609-ea202d871acf/sys/
+#comment out contents of /opt/devel/rpi/prepare-chroot.sh on rpi filesystem
